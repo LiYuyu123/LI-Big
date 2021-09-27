f18d80a5a1bd88df4d920d5d4dc8d5dd2909f500
LiYu <2248868140@qq.com>
2021-09-28 01:09:44 +0800

    完成charts3

--- src/component/Chart-3.tsx
+++ src/component/Chart-3.tsx
@@ -6,14 +6,45 @@ const Chart3=()=>{
     const px=(n)=>n / 1502 * (window as any).pageWidth
     useEffect(()=>{
         let myChart = echarts.init(ref.current);
-
-
+        myChart.setOption({
+            color: ['#8D70F8', '#33A4FA'],
+            xAxis: {show: false},
+            yAxis: {show: false},
+            legend: {show: false},
+            series: [
+                {
+                    name: '访问来源',
+                    type: 'pie',
+                    radius: ['75%', '90%'],
+                    avoidLabelOverlap: false,
+                    label: {
+                        show: true, position: 'inside', textStyle: {color: 'white', fontSize: px(20)},
+                        formatter(options) {
+                            return options.value * 100 + '%';
+                        }
+                    },
+                    labelLine: {show: false},
+                    itemStyle: {
+                        borderColor: '#0F113A',
+                        borderWidth: px(4)
+                    },
+                    data: [
+                        {value: 0.2, name: '女'},
+                        {value: 0.8, name: '男'},
+                    ]
+                }
+            ]
+        });
     },[])
     return(
-        <div className="bordered 时段">
-            <h1>案发时段分析</h1>
+        <div className="bordered 访问">
+            <h1>访问来源</h1>
             <div ref={ref} className='chart'>
 
+            </div>
+            <div className="legend">
+                <span className="male"/>男
+                <span className="female"/>女
             </div>
         </div>
     )
